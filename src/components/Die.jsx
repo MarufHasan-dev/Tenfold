7a8ad6cc08841c8307b5c3af4bbd4bfe19bee837
MarufHasan-dev <170166811+MarufHasan-dev@users.noreply.github.com>
2025-04-25 19:26:44 +0600

    added real dice instead of numbers

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,7 +1,24 @@
+import Die_1 from "../assets/images/dice_1.png";
+import Die_2 from "../assets/images/dice_2.png";
+import Die_3 from "../assets/images/dice_3.png";
+import Die_4 from "../assets/images/dice_4.png";
+import Die_5 from "../assets/images/dice_5.png";
+import Die_6 from "../assets/images/dice_6.png";
+
 export default function Die(props) {
   const styles = {
     backgroundColor: props.isHeld ? " #59e391" : "#ffffff",
   };
+
+  const diceImages = {
+    1: Die_1,
+    2: Die_2,
+    3: Die_3,
+    4: Die_4,
+    5: Die_5,
+    6: Die_6,
+  };
+
   return (
     <button
       style={styles}
@@ -12,7 +29,7 @@ export default function Die(props) {
         props.isHeld ? "held" : "not held"
       }`}
     >
-      {props.value}
+      <img className="dice-icons" src={diceImages[props.value]} alt="" />
     </button>
   );
 }
